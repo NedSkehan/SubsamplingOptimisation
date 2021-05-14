7632f89d4edc6eadd11f2a86e5836caff978faec
NedSkehan <ned.skehan@usq.edu.au>
2021-05-14 10:10:00 +1000

    install finished

--- R/rep.function.R
+++ R/rep.function.R
@@ -6,28 +6,29 @@
 #' @param depth_spdf SpatialPointsDataFrame of data
 #' @param grid grid to krige across
 #' @param replications numeric of required replications
+#' @param property char of property name in 'depth_spdf' for kriging. Case sensitive
 #'
 #' @return list of error indicies for each subsample size
 #' @export
 #' @examples
-#' rep.function(vec,spdf,grid)
 
 
-rep.function <- function(css,depth_spdf,grid,replications) {
+
+rep.function <- function(css,depth_spdf,grid,replications,property) {
 
   error.vec <- data.frame(css = css,
                           rmse = rep(NA, length(css)),
                           mse = rep(NA, length(css)),
                           mae = rep(NA, length(css)),
-                          rsq = rep(NA, length(css)))
+                          rsq = rep(NA, length(css)),
+                          rsq_adj = rep(NA, length(css)),
+                          sde = rep(NA, length(css)))
 
   error.dist <- list()
 
   ct <- 0
 
-  #cl <- parallel::makeCluster(numcores)
-  #doParallel::registerDoParallel(cl)
-  #rep.list <-
+
   for (s in css) {
     # create a list of all repetitions
     set.seed(s)
@@ -39,7 +40,9 @@ rep.function <- function(css,depth_spdf,grid,replications) {
                              rmse = rep(NA,replications),
                              mse = rep(NA,replications),
                              mae = rep(NA,replications),
-                             rsq = rep(NA,replications))
+                             rsq = rep(NA,replications),
+                             rsq_adj = rep(NA,replications),
+                             sde = rep(NA,replications))
 
 
 
@@ -52,12 +55,14 @@ rep.function <- function(css,depth_spdf,grid,replications) {
 
       # this produces a data frame with errors of the krige estimate, for the single layer,
       # at the single rep
-      depth <- OK_krig(subsample.cal,grid,subsample.val)
+      depth <- OK_krig(subsample.cal,grid,subsample.val,property)
 
       error.reps$rmse[j] <- depth$rmse
       error.reps$mse[j] <- depth$mse
       error.reps$mae[j] <- depth$mae
       error.reps$rsq[j] <- depth$rsq
+      error.reps$rsq_adj[j] <- depth$rsq_adj
+      error.reps$sde <- depth$sde
     }
 
     ct <- ct + 1
@@ -65,11 +70,12 @@ rep.function <- function(css,depth_spdf,grid,replications) {
     error.vec$mse[ct] <- mean(error.reps$mse)
     error.vec$mae[ct] <- mean(error.reps$mae)
     error.vec$rsq[ct] <- mean(error.reps$rsq)
+    error.vec$rsq_adj[ct] <- mean(error.reps$rsq_adj)
+    error.vec$sde <- mean(error.reps$sde)
 
     error.dist[[ct]] <- error.reps
 
   }
-  #parallel::stopCluster(cl)
 
   names(error.dist) <- css
   output <- list(error.vec,error.dist)
@@ -84,73 +90,62 @@ rep.function <- function(css,depth_spdf,grid,replications) {
 #' @param data SpatialPointsDataFrame of calibration data
 #' @param grid grid for kriging
 #' @param data.val SpatialPointsDataFrame of validation data
-#' @param proj4Str chr string of the projection of data
-#' @param l numeric column index of variable for kriging
+#' @param property char of property name in 'data' for kriging. Case sensitive
 #'
 #' @return list of error indicies for each subsample size
 #' @export
 #' @examples
-#' rep.function.rk(vec,spdf,grid)
 
 
-OK_krig <- function(data,grid,data.val,proj4Str,l) {
 
-  data.df <- as.data.frame(data)
-  data.df <- stats::na.omit(data.df)
+OK_krig <- function(data,grid,data.val,property) {
 
-  # initialise data frame for prediction results
-  #preds <- as.data.frame(matrix(c(rep(NA,1)),nrow = nrow(data.val),ncol = 1))
-  #names(preds) <- "layer"
+    l <- grep("property",colnames(data))
 
-  error.temp <- data.frame(rep = seq(1,1,1),rmse = rep(NA,1),
-                           mse = rep(NA,1),mae = rep(NA,1),
-                           rsq = rep(NA,1))
+    data.df <- as.data.frame(data)
+    data.df <- stats::na.omit(data.df)
+
+    # initialise data frame for prediction results
+    error.temp <- data.frame(rep = seq(1,1,1),rmse = rep(NA,1),
+                             mse = rep(NA,1),mae = rep(NA,1),
+                             rsq = rep(NA,1),rsq_adj = rep(NA,1),
+                             sde = rep(NA,1))
 
 
-  # variogram
-  vgm <- automap::autofitVariogram(data.df[,l]~1,data,model = c("Sph","Exp","Gau","Ste"))
-  #plot(vgm)
+    # variogram
+    vgm <- automap::autofitVariogram(data.df[,l]~1,data,model = c("Sph","Exp","Gau","Ste"))
+    #plot(vgm)
 
-  # ordinary kriging
-  dat.krig <- gstat::krige(data.df[,l]~1,data,grid,model = vgm$var_model)
-  #plot(dat.krig$var1.pred)
+    ######################## Just for a visual ############################
+    # ordinary kriging
+    #dat.krig <- krige(data.df[,l]~1,data,newdata = grid,model = vgm$var_model)
 
-  # convert to SPDF to extract points on the grid
-  map.df <- as.data.frame(dat.krig)
-  #map.spdf <- SpatialPointsDataFrame(coords=map.df[,1:2], data = map.df, proj4string = crs(proj4Str))
+    # convert to SPDF to extract points on the grid
+    #map.df <- as.data.frame(dat.krig)
 
-  # convert to raster to plot
-  kriged.map <- raster::rasterFromXYZ(map.df,crs = sp::CRS(proj4Str))
-  raster::plot(kriged.map$var1.pred)
-  raster::plot(data,add = T,pch = 3,col="red")
-  raster::plot(data.val,add = T,pch = 1)
-  graphics::legend("topright",inset=0.01,legend = c("OK Cal","Val"),col = c("red","black"),cex = 0.8,pch=c(3,1))
+    # convert to raster to plot
+    #kriged.map <- rasterFromXYZ(map.df,crs = CRS(proj4Str))
+    #plot(kriged.map$var1.pred)
+    #plot(data,add = T,pch = 3,col="red")
+    #plot(data.val,add = T,pch = 1)
+    #legend("topright",inset=0.01,legend = c("OK Cal","Val"),col = c("red","black"),cex = 0.8,pch=c(3,1))
 
-  output.pred <- kriged.map$var1.pred
-  output.var <- kriged.map$var1.var
+    #output.pred <- kriged.map$var1.pred
+    #output.var <- kriged.map$var1.var
 
-  #Create raster stack of kriged layers
-  #if(map.idx==1){
-  #output <- output.pred
-  #}else{
-  #  output <- stack(output,output.pred)
-  #}
+    ######################## END: Just for a visual ############################
 
-  #cat("Layer",layers[id],"is complete","\n")
+    ##### calculate predicted values
+    prediction <- as.data.frame(gstat::krige(data.df[,l]~1,data,model=vgm$var_model,newdata = data.val))
+    preds <- prediction[,3]
 
-  ##### calculate predicted values
-  prediction <- as.data.frame(gstat::krige(data.df[,l]~1,data,model=vgm$var_model,newdata = data.val))
-  preds <- prediction[,3]
+    # temp df for storing each error
+    error.temp$rmse <- rmse(data.val@data[[l]],preds)
+    error.temp$mse <- mse(data.val@data[[l]],preds)
+    error.temp$mae <- mae(data.val@data[[l]],preds)
+    error.temp$rsq <- rsq(data.val@data[[l]],preds)
+    error.temp$rsq_adj <- rsq_adj(data.val@data[[l]],preds,nrow(data.df))
+    error.temp$sde <- sde(data.val@data[[l]],preds)
 
-  # temp df for storing each error
-  error.temp$rmse <- rmse(data.val@data[[l]],preds)
-  error.temp$mse <- mse(data.val@data[[l]],preds)
-  error.temp$mae <- mae(data.val@data[[l]],preds)
-  error.temp$rsq <- rsq(data.val@data[[l]],preds)
-
-  # compile the output
-  #names(output) <- layers
-  #output.list <- list(output.pred,preds)
-
-  return(error.temp)
-}
+    return(error.temp)
+  }
--- R/rmse.R
+++ R/rmse.R
@@ -8,7 +8,7 @@
 #' @return numeric of RMSE
 #' @export
 #' @examples
-#' rmse(actual,predicted)
+
 
 rmse <- function(actual,predicted) {
   sqrt(mean((actual - predicted)^2))
@@ -24,7 +24,7 @@ rmse <- function(actual,predicted) {
 #' @return numeric of MAE
 #' @export
 #' @examples
-#' mae(actual,predicted)
+
 
 mae <- function(actual,predicted) {
   sum(abs(actual-predicted))/length(actual)
@@ -40,7 +40,7 @@ mae <- function(actual,predicted) {
 #' @return numeric of MSE
 #' @export
 #' @examples
-#' mse(actual,predicted)
+
 
 mse <- function(actual,predicted) {
   sum((actual-predicted)^2)/length(actual)
@@ -56,8 +56,41 @@ mse <- function(actual,predicted) {
 #' @return numeric of RSQ
 #' @export
 #' @examples
-#' rsq(actual,predicted)
+
 
 rsq <- function(actual,predicted) {
   stats::cor(actual,predicted)^2
+}
+
+#' A function for calculating Standard Deviation of residuals
+#'
+#' This function will calculate SDE
+#'
+#' @param actual vector of validation data
+#' @param predicted vector of predicted data
+#'
+#' @return numeric of SDE
+#' @export
+#' @examples
+
+
+sde <- function(actual,predicted) {
+  stats::sd(actual-predicted)
+}
+
+#' A function for calculating Adjusted Rsquared
+#'
+#' This function will calculate RSQ_ADJ
+#'
+#' @param actual vector of validation data
+#' @param predicted vector of predicted data
+#' @param p numeric of number of predictors used
+#'
+#' @return numeric of RSQ
+#' @export
+#' @examples
+
+
+rsq_adj <- function(actual,predicted,p) {
+  1-(1-(stats::cor(actual,predicted)^2))*((p-1)/(p-length(predicted)-1))
 }
